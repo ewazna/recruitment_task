cf11a8e87db35da8b3fbbaf77fc8de6b140e5e62
ewazna <e.m.wazna@gmail.com>
2025-05-08 22:38:41 +0200

    simplify data fetching states

--- src/pages/ProductItemPage/ProductItemPage.tsx
+++ src/pages/ProductItemPage/ProductItemPage.tsx
@@ -1,44 +1,37 @@
 import { useParams } from "react-router-dom";
-import { useEffect, useState, type JSX } from "react";
 import { useFetchProductsQuery } from "../../store";
 import ProductDetails from "./ProductDetails";
 import Spinner from "../../components/Spinner/Spinner";
 
 function ProductItemPage() {
-  const [content, setContent] = useState<JSX.Element | null>(<Spinner />);
-
   const { productId } = useParams();
   const productsFetchingResult = useFetchProductsQuery();
   const products = productsFetchingResult.data;
 
-  useEffect(() => {
-    if (productsFetchingResult.isFetching) {
-      setContent(<Spinner />);
-    } else if (productsFetchingResult.isError) {
-      setContent(
+  if (productsFetchingResult.isFetching) {
+    return <Spinner />;
+  } else if (productsFetchingResult.isError) {
+    return (
+      <div className="d-flex flex-column justify-content-center align-items-center w-100 min-vh-100">
+        <img alt="error_image" src="/error.png" className="w-25 h-25" />
+        <p className="mt-2">Error during product fetching</p>
+      </div>
+    );
+  } else if (productsFetchingResult.isSuccess && products) {
+    const product = products.find(
+      (product) => product.id.toString() === productId
+    );
+
+    if (product) {
+      return <ProductDetails product={product} />;
+    } else {
+      return (
         <div className="d-flex flex-column justify-content-center align-items-center w-100 min-vh-100">
-          <img alt="error_image" src="/error.png" className="w-25 h-25" />
-          <p className="mt-2">Error during product fetching</p>
+          <p>Product not found</p>
         </div>
       );
-    } else if (productsFetchingResult.isSuccess && products) {
-      const product = products.find(
-        (product) => product.id.toString() === productId
-      );
-
-      if (product) {
-        setContent(<ProductDetails product={product} />);
-      } else {
-        setContent(
-          <div>
-            <p>Product not found</p>
-          </div>
-        );
-      }
     }
-  }, [productsFetchingResult, productId, products]);
-
-  return <>{content}</>;
+  }
 }
 
 export default ProductItemPage;
--- src/pages/ProductsPage/ProductsPage.tsx
+++ src/pages/ProductsPage/ProductsPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, type JSX } from "react";
+import { useState } from "react";
 import {
   TiArrowUnsorted,
   TiArrowSortedDown,
@@ -13,7 +13,6 @@ import Spinner from "../../components/Spinner/Spinner";
 
 function ProductsPage() {
   const [sortType, setSortType] = useState(SortType.Default);
-  const [content, setContent] = useState<JSX.Element | null>(<Spinner />);
 
   const sortingOptions = [
     {
@@ -46,86 +45,79 @@ function ProductsPage() {
   const productsFetchingResult = useFetchProductsQuery();
   const products = productsFetchingResult.data;
 
-  useEffect(() => {
-    if (productsFetchingResult.isFetching) {
-      setContent(<Spinner />);
-    } else if (productsFetchingResult.isError) {
-      setContent(
-        <div className="d-flex flex-column justify-content-center align-items-center w-100 min-vh-100">
-          <img alt="error_image" src="/error.png" className="w-25 h-25" />
-          <p className="mt-2">Error during product fetching</p>
-        </div>
-      );
-    } else if (productsFetchingResult.isSuccess && products) {
-      if (products) {
-        const { label: sortingLabel, icon: sortingIcon } = sortingOptions.find(
-          (option) => option.sortType === sortType
-        )!;
+  if (productsFetchingResult.isFetching) {
+    return <Spinner />;
+  } else if (productsFetchingResult.isError) {
+    return (
+      <div className="d-flex flex-column justify-content-center align-items-center w-100 min-vh-100">
+        <img alt="error_image" src="/error.png" className="w-25 h-25" />
+        <p className="mt-2">Error during product fetching</p>
+      </div>
+    );
+  } else if (productsFetchingResult.isSuccess && products) {
+    const { label: sortingLabel, icon: sortingIcon } = sortingOptions.find(
+      (option) => option.sortType === sortType
+    )!;
 
-        const sortedProducts = [...products];
-        switch (sortType) {
-          case SortType.Default:
-            break;
-          case SortType.TitleAsc:
-            sortedProducts.sort((a, b) => a.title.localeCompare(b.title));
-            break;
-          case SortType.TitleDesc:
-            sortedProducts.sort((a, b) => b.title.localeCompare(a.title));
-            break;
-          case SortType.PriceAsc:
-            sortedProducts.sort((a, b) => a.price - b.price);
-            break;
-          case SortType.PriceDesc:
-            sortedProducts.sort((a, b) => b.price - a.price);
-            break;
-          default:
-            break;
-        }
-
-        setContent(
-          <main className="mt-5 pt-2">
-            <ContextMenu
-              name={`Sorting: ${sortingLabel}`}
-              icon={sortingIcon}
-              className="context-menu"
-            >
-              {sortingOptions.map((option) => {
-                return (
-                  <div key={option.sortType}>
-                    <Button
-                      onClick={() => {
-                        setSortType(option.sortType);
-                      }}
-                      active={option.sortType === sortType}
-                      className="w-100"
-                    >
-                      {option.label} {option.icon}
-                    </Button>
-                  </div>
-                );
-              })}
-            </ContextMenu>
-            <ul>
-              {sortedProducts.map((product) => {
-                return (
-                  <li key={product.id}>
-                    <ProductItem
-                      id={product.id}
-                      image={product.image}
-                      title={product.title}
-                      price={product.price}
-                    />
-                  </li>
-                );
-              })}
-            </ul>
-          </main>
-        );
-      }
+    const sortedProducts = [...products];
+    switch (sortType) {
+      case SortType.Default:
+        break;
+      case SortType.TitleAsc:
+        sortedProducts.sort((a, b) => a.title.localeCompare(b.title));
+        break;
+      case SortType.TitleDesc:
+        sortedProducts.sort((a, b) => b.title.localeCompare(a.title));
+        break;
+      case SortType.PriceAsc:
+        sortedProducts.sort((a, b) => a.price - b.price);
+        break;
+      case SortType.PriceDesc:
+        sortedProducts.sort((a, b) => b.price - a.price);
+        break;
+      default:
+        break;
     }
-  }, [productsFetchingResult, sortType, products]);
 
-  return <>{content}</>;
+    return (
+      <main className="mt-5 pt-2">
+        <ContextMenu
+          name={`Sorting: ${sortingLabel}`}
+          icon={sortingIcon}
+          className="context-menu"
+        >
+          {sortingOptions.map((option) => {
+            return (
+              <div key={option.sortType}>
+                <Button
+                  type="button"
+                  onClick={() => setSortType(option.sortType)}
+                  active={option.sortType === sortType}
+                  className="w-100"
+                >
+                  {option.label} {option.icon}
+                </Button>
+              </div>
+            );
+          })}
+        </ContextMenu>
+        <ul>
+          {sortedProducts.map((product) => {
+            return (
+              <li key={product.id}>
+                <ProductItem
+                  id={product.id}
+                  image={product.image}
+                  title={product.title}
+                  price={product.price}
+                />
+              </li>
+            );
+          })}
+        </ul>
+      </main>
+    );
+  }
 }
 
 export default ProductsPage;
